7ea3ffffea660c2c1721a83bfc5ee259e8f0638d
Sejimu <snursultan322@gmail.com>
2023-08-07 18:21:58 +0600

    authorization

---
+++ src/routes/MainRoute.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import AuthPage from "../pages/AuthPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import MainLayout from "../layouts/MainLayout";
+import ActivationPage from "../pages/ActivationPage";
+
+function MainRoute() {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+      </Route>
+      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/api/account/activate/" element={<ActivationPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
+
+export default MainRoute;
---
+++ src/utils/axios.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+import { BASE_URL } from "./consts";
+
+const $axios = axios.create();
+
+$axios.interceptors.request.use(
+  async (config) => {
+    const tokens = JSON.parse(localStorage.getItem("tokens"));
+    if (tokens) {
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${tokens.access}`,
+      };
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+$axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const config = error.config;
+    if (error.response) {
+      if (error.response.status === 401 && !config._retry) {
+        config._retry = true;
+        const access = await refreshAccessToken();
+        axios.defaults.headers.common["Authorization"] = `Bearer ${access}`;
+        return $axios(config);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+async function refreshAccessToken() {
+  try {
+    const tokens = JSON.parse(localStorage.getItem("tokens"));
+
+    if (tokens) {
+      const { data } = await axios.post(`${BASE_URL}/account/refresh/`, {
+        refresh: tokens.refresh,
+      });
+
+      localStorage.setItem(
+        "tokens",
+        JSON.stringify({
+          access: data.access,
+          refresh: data.refresh,
+        })
+      );
+      return data.access;
+    }
+  } catch (e) {
+    localStorage.removeItem("tokens");
+  }
+}
+
+export default $axios;
