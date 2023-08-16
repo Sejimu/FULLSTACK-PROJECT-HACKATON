25da182cad9dda3389e0c99a25055e8af306612d
Sejimu <snursultan322@gmail.com>
2023-08-16 14:21:26 +0600

    fixed getComments

--- src/pages/LessonPage.jsx
+++ src/pages/LessonPage.jsx
@@ -23,7 +23,7 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 import { useAuthContext } from "../contexts/AuthContext";
-import { getConfetti } from "../utils/confetti";
+// import { getConfetti } from "../utils/confetti";
 
 const theme = createTheme({
   typography: {
@@ -57,7 +57,6 @@ const LessonPage = () => {
     dislike,
     isLiked,
     isDisliked,
-    comments,
     getComments,
     comments2,
     setComments2,
@@ -75,7 +74,6 @@ const LessonPage = () => {
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
-
   const handleClose = () => {
     setAnchorEl(null);
   };
@@ -87,19 +85,14 @@ const LessonPage = () => {
   }, [comBtn, searchParams]);
 
   useEffect(() => {
-    setComments2([]);
-    setComBtn(false);
     getLessons();
+    setComBtn(false);
     setIsAnswerCorrect(null);
-    // getComments(1);
     document.body.classList.add("lessonPage");
     return () => {
       document.body.classList.remove("lessonPage");
     };
   }, [searchParams]);
-
-  console.log(comments2);
-
   useEffect(() => {
     const currentParams = Object.fromEntries([...searchParams]);
     setSearchParams({ ...currentParams, course_id: id, lesson: page });
@@ -114,14 +107,6 @@ const LessonPage = () => {
 
   return (
     <>
-      {lessons.map((item) => {
-        return (
-          <ThemeProvider theme={theme}>
-            <Container component="main" sx={{ color: "white", width: "90%" }}>
-              <Box>
-                <IconButton
-                  onClick={handleClick}
-                  sx={{ marginLeft: "96%", color: "white" }}
       {lessons.map((item) => (
         <ThemeProvider theme={theme}>
           <Container component="main" sx={{ color: "white", width: "90%" }}>
@@ -154,23 +139,17 @@ const LessonPage = () => {
                     navigate(`/courses`);
                   }}
                 >
-                  <ExtensionIcon />
-                </IconButton>
-                <Menu
-                  id="basic-menu"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  MenuListProps={{
-                    "aria-labelledby": "basic-button",
+                  Удалить урок
+                </MenuItem>
+                <MenuItem
+                  onClick={() => deleteQuestion(item.id)}
+                  component={Button}
+                  sx={{
+                    textTransform: "capitalize",
+                    width: "100%",
+                    color: "red",
                   }}
                 >
-                  <MenuItem
-                    component={Button}
-                    sx={{ textTransform: "capitalize", color: "red" }}
-                    onClick={() => {
-                      deleteLesson(item.id);
-                      navigate(`/courses`);
                   Удалить задание
                 </MenuItem>
                 <MenuItem
@@ -244,7 +223,7 @@ const LessonPage = () => {
                   selectedAnswer === item.right_answer
                 ) {
                   setIsAnswerCorrect(true);
-                  getConfetti();
+                  // getConfetti();
                 } else {
                   setIsAnswerCorrect(false);
                 }
@@ -338,134 +317,22 @@ const LessonPage = () => {
                       fontSize: "24px",
                     }}
                   >
-                    Удалить урок
-                  </MenuItem>
-                  <MenuItem
-                    onClick={() => deleteQuestion(item.id)}
-                    component={Button}
+                    Верный ответ! Продолжайте в том же духе.
+                  </Typography>
+                )}
+                {isAnswerCorrect === false && (
+                  <Typography
                     sx={{
-                      textTransform: "capitalize",
-                      width: "100%",
                       color: "red",
+                      marginTop: "10px",
+                      textAlign: "center",
+                      fontSize: "24px",
                     }}
                   >
-                    Удалить задание
-                  </MenuItem>
-                  <MenuItem
-                    onClick={() => navigate(`/courses/editlesson/${item.id}`)}
-                    component={Button}
-                    sx={{ textTransform: "capitalize", width: "100%" }}
-                  >
-                    Изменить урок
-                  </MenuItem>
-                  <MenuItem
-                    onClick={() => navigate(`/lesson/${item.id}/question`)}
-                    component={Button}
-                    sx={{ textTransform: "capitalize", width: "100%" }}
-                  >
-                    Добавить задание
-                  </MenuItem>
-                </Menu>
+                    Неправильный ответ. Попробуйте снова.
+                  </Typography>
+                )}
               </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "flex-end",
-                  padding: 0,
-                  margin: 0,
-                }}
-              >
-                <Pagination
-                  count={totalLessons}
-                  page={page}
-                  onChange={(_, val) => setPage(val)}
-                  variant="outlined"
-                  color="secondary"
-                />
-              </Box>
-              <Box sx={{ margin: "5% auto" }}>
-                <Typography component="h1" variant="h4">
-                  Тема урока: {item.title}
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  margin: "5%",
-                  display: "flex",
-                  gap: "5%",
-                  alignItems: "center",
-                }}
-              >
-                <Typography
-                  component="p"
-                  variant="p"
-                  sx={{ textAlign: "left", width: "45%" }}
-                >
-                  {item.body}
-                </Typography>
-                <iframe
-                  width="560"
-                  height="315"
-                  src={item.youtube_link}
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowfullscreen
-                ></iframe>
-              </Box>
-              <Box
-                component="form"
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  if (
-                    inpVal.toLowerCase() === item.right_answer.toLowerCase() ||
-                    selectedAnswer === item.right_answer
-                  ) {
-                    setIsAnswerCorrect(true);
-                  } else {
-                    setIsAnswerCorrect(false);
-                  }
-                  setInpVal("");
-                }}
-                sx={{
-                  margin: "5% auto",
-                  textAlign: "center",
-                  width: "60%",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-              >
-                <Typography
-                  component="h1"
-                  variant="h4"
-                  sx={{ marginBottom: "3%" }}
-                >
-                  Задание:
-                </Typography>
-                <Typography
-                  component="h3"
-                  variant="h6"
-                  sx={{ marginBottom: "3%" }}
-                >
-                  {item.question}
-                </Typography>
-                {!item.wrong_answers ? (
-                  <TextField
-                    label="Введите ваш ответ"
-                    variant="outlined"
-                    value={inpVal}
-                    onChange={(e) => setInpVal(e.target.value)}
-                    sx={{
-                      marginBottom: "3%",
-                      width: "100%",
-                      color: "white",
-                      background: "transparent",
-                      border: "1px solid white",
-                      borderRadius: "5px",
-                      "& label": {
-                        color: "white",
-                      },
             </Box>
             <Box
               sx={{
@@ -503,164 +370,62 @@ const LessonPage = () => {
                     onClick={() => {
                       like(item.title, item.id);
                     }}
-                    inputProps={{
-                      style: {
-                        color: "white",
-                      },
+                  />
+                ) : (
+                  <ThumbUpOutlinedIcon
+                    sx={{ color: "white", fontSize: "34px" }}
+                    onClick={() => {
+                      like(item.title, item.id);
+                    }}
+                  />
+                )}
+              </IconButton>
+              <Typography component="h3" variant="h6">
+                {likes}
+              </Typography>
+              <IconButton>
+                {isDisliked ? (
+                  <ThumbDownIcon
+                    sx={{ color: "white", fontSize: "34px" }}
+                    onClick={() => {
+                      dislike(item.title, item.id);
                     }}
                   />
                 ) : (
-                  <FormControl>
-                    <RadioGroup
-                      aria-labelledby="demo-radio-buttons-group-label"
-                      name="radio-buttons-group"
-                      value={selectedAnswer}
-                      onChange={(e) => setSelectedAnswer(e.target.value)}
-                      sx={{ textAlign: "left" }}
-                    >
-                      <FormControlLabel
-                        value={item.right_answer}
-                        control={<Radio />}
-                        label={item.right_answer}
-                      />
-                      {item.wrong_answers.split(", ").map((i) => (
-                        <FormControlLabel
-                          value={i}
-                          control={<Radio />}
-                          label={i}
-                        />
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
+                  <ThumbDownOffAltOutlinedIcon
+                    sx={{ color: "white", fontSize: "34px" }}
+                    onClick={() => {
+                      dislike(item.title, item.id);
+                    }}
+                  />
                 )}
-
-                <Button
-                  type="submit"
-                  variant="contained"
-                  sx={{ color: "#9021cf", width: "30%" }}
-                >
-                  Отправить
-                </Button>
-                <Box>
-                  {isAnswerCorrect === true && (
-                    <Typography
-                      sx={{
-                        color: "green",
-                        marginTop: "10px",
-                        textAlign: "center",
-                        fontSize: "24px",
-                      }}
-                    >
-                      Верный ответ! Продолжайте в том же духе.
-                    </Typography>
-                  )}
-                  {isAnswerCorrect === false && (
-                    <Typography
-                      sx={{
-                        color: "red",
-                        marginTop: "10px",
-                        textAlign: "center",
-                        fontSize: "24px",
-                      }}
-                    >
-                      Неправильный ответ. Попробуйте снова.
-                    </Typography>
-                  )}
-                </Box>
-              </Box>
-              <Box
-                sx={{
-                  margin: "5% auto",
-                  display: "flex",
-                  justifyContent: "space-between",
-                }}
-              >
-                {item.id !== 1 ? (
-                  <Button variant="outlined" onClick={prevPage}>
-                    ⇽ Предыдущее задание
-                  </Button>
-                ) : (
-                  ""
-                )}
-                {item.id < totalLessons ? (
-                  <Button variant="outlined" onClick={nextPage}>
-                    Следующее задание ⇾
-                  </Button>
-                ) : (
-                  ""
-                )}
-              </Box>
-              <Box
-                sx={{
-                  margin: "5% auto",
-                  display: "flex",
-                  alignItems: "center",
-                }}
-              >
-                <IconButton>
-                  {isLiked ? (
-                    <ThumbUpIcon
-                      sx={{ color: "white", fontSize: "34px" }}
-                      onClick={() => {
-                        like(item.title, item.id);
-                      }}
-                    />
-                  ) : (
-                    <ThumbUpOutlinedIcon
-                      sx={{ color: "white", fontSize: "34px" }}
-                      onClick={() => {
-                        like(item.title, item.id);
-                      }}
-                    />
-                  )}
-                </IconButton>
-                <Typography component="h3" variant="h6">
-                  {likes}
-                </Typography>
-                <IconButton>
-                  {isDisliked ? (
-                    <ThumbDownIcon
-                      sx={{ color: "white", fontSize: "34px" }}
-                      onClick={() => {
-                        dislike(item.title, item.id);
-                      }}
-                    />
-                  ) : (
-                    <ThumbDownOffAltOutlinedIcon
-                      sx={{ color: "white", fontSize: "34px" }}
-                      onClick={() => {
-                        dislike(item.title, item.id);
-                      }}
-                    />
-                  )}
-                </IconButton>
-                <Typography component="h3" variant="h6">
-                  {dislikes}
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  margin: "5% auto",
-                  display: "flex",
-                  alignItems: "center",
-                }}
-              >
-                <Typography component="h3" variant="h4">
-                  Комментарии:
-                </Typography>
-              </Box>
-              <Button
-                onClick={() => {
-                  setComBtn(item.id);
-                }}
-              >
-                Показать Комментарии
-                {comBtn && comments2.map((value) => <h1>{value.body}</h1>)}
-              </Button>
-            </Container>
-          </ThemeProvider>
-        );
-      })}
+              </IconButton>
+              <Typography component="h3" variant="h6">
+                {dislikes}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                margin: "5% auto",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Typography component="h3" variant="h4">
+                Комментарии:
+              </Typography>
+            </Box>
+            <Button
+              onClick={() => {
+                setComBtn(item.id);
+              }}
+            >
+              Показать Комментарии
+              {comBtn && comments2.map((value) => <h1>{value.body}</h1>)}
+            </Button>
+          </Container>
+        </ThemeProvider>
+      ))}
     </>
   );
 };
